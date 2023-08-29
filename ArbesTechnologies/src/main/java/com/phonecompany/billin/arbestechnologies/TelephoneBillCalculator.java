a379485d0685b620a63742849e8846447b336089
TomasMacoun <macount@seznam.cz>
2023-08-29 15:02:29 +0200

    Added Exception to the interface

--- ArbesTechnologies/src/main/java/com/phonecompany/billin/arbestechnologies/TelephoneBillCalculator.java
+++ ArbesTechnologies/src/main/java/com/phonecompany/billin/arbestechnologies/TelephoneBillCalculator.java
@@ -1,7 +1,8 @@
 package com.phonecompany.billin.arbestechnologies;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 
 public interface TelephoneBillCalculator {
-    BigDecimal calculate(String phoneLog);
+    BigDecimal calculate(String phoneLog) throws IOException;
 }
