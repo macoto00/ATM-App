95aa1264bc7448ce1bbe16c0c685a6b58ba0aef9
TomasMacoun <macount@seznam.cz>
2023-08-29 14:28:05 +0200

    Initial commit

---
+++ ArbesTechnologies/src/main/java/com/phonecompany/billin/arbestechnologies/ArbesTechnologiesApplication.java
@@ -0,0 +1,13 @@
+package com.phonecompany.billin.arbestechnologies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArbesTechnologiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArbesTechnologiesApplication.class, args);
+	}
+
+}
