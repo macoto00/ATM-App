a650b0a8f93aa84b510eea9257bb2d969d6e84d5
TomasMacoun <macount@seznam.cz>
2023-08-31 10:44:12 +0200

    Delete TelephoneBillCalculator.java

--- ArbesTechnologies/src/main/java/com/phonecompany/billin/arbestechnologies/TelephoneBillCalculator.java
+++
@@ -1,8 +0,0 @@
-package com.phonecompany.billin.arbestechnologies;
-
-import java.io.IOException;
-import java.math.BigDecimal;
-
-public interface TelephoneBillCalculator {
-    BigDecimal calculate(String phoneLog) throws IOException;
-}
