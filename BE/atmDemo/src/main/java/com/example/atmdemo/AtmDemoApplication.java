2617c91f4553d65199f044349b53fdfca9bda1a8
TomasMacoun <macount@seznam.cz>
2023-09-20 21:34:58 +0200

    initial commit for BE

---
+++ BE/atmDemo/src/main/java/com/example/atmdemo/AtmDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.atmdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtmDemoApplication.class, args);
+    }
+
+}
---
+++ BE/atmDemo/src/test/java/com/example/atmdemo/AtmDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atmdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
