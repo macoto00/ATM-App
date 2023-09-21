e38158e2a3d856183f0381ab673b8276ff8b4cc7
TomasMacoun <macount@seznam.cz>
2023-09-21 15:15:37 +0200

    Update TransactionRepository.java

--- BE/atmDemo/src/main/java/com/example/atmdemo/repositories/TransactionRepository.java
+++ BE/atmDemo/src/main/java/com/example/atmdemo/repositories/TransactionRepository.java
@@ -1,8 +1,9 @@
 package com.example.atmdemo.repositories;
 
+import com.example.atmdemo.models.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TransactionRepository extends JpaRepository {
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 }
