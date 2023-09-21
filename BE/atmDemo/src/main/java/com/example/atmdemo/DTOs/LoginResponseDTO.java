1292d98fdec318c683838b98c09236959793f377
TomasMacoun <macount@seznam.cz>
2023-09-21 19:08:40 +0200

    Create LoginResponseDTO.java

---
+++ BE/atmDemo/src/main/java/com/example/atmdemo/DTOs/LoginResponseDTO.java
@@ -0,0 +1,12 @@
+package com.example.atmdemo.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponseDTO {
+    private String email;
+}
