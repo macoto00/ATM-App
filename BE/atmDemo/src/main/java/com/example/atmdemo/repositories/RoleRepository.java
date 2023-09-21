21217b625eeedc1a16b558410161780080b54ded
TomasMacoun <macount@seznam.cz>
2023-09-21 19:08:56 +0200

    Create RoleRepository.java

---
+++ BE/atmDemo/src/main/java/com/example/atmdemo/repositories/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.example.atmdemo.repositories;
+
+import com.example.atmdemo.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
