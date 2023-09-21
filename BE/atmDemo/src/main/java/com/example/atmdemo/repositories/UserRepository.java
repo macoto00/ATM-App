c49118438a3c54f3d9f0307d245d36ef58fb30d5
TomasMacoun <macount@seznam.cz>
2023-09-21 15:15:34 +0200

    Update UserRepository.java

--- BE/atmDemo/src/main/java/com/example/atmdemo/repositories/UserRepository.java
+++ BE/atmDemo/src/main/java/com/example/atmdemo/repositories/UserRepository.java
@@ -1,8 +1,11 @@
 package com.example.atmdemo.repositories;
 
+import com.example.atmdemo.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
+    boolean existsByNickname(String nickname);
 }
