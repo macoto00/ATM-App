b58edb274bad1b3dc3043bb5d3ca54e7d6690e7f
TomasMacoun <macount@seznam.cz>
2023-09-21 19:08:52 +0200

    Update JwtAuthenticationFilter.java

--- BE/atmDemo/src/main/java/com/example/atmdemo/security/JWT/JwtAuthenticationFilter.java
+++ BE/atmDemo/src/main/java/com/example/atmdemo/security/JWT/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.example.atmdemo.security.JWT;
 
+import com.example.atmdemo.security.CustomUserDetailsService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,11 +15,15 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+
 @NoArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Autowired
     private JwtUtils jwtUtils;
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
     @Override
     protected void doFilterInternal(
             HttpServletRequest request,
@@ -27,13 +32,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     ) throws ServletException, IOException {
         String token = jwtUtils.getJwtFromCookies(request);
         String refreshToken = jwtUtils.getRefreshJwtFromCookies(request);
+
         if (token == null && refreshToken != null && jwtUtils.validateJwtToken(refreshToken)) {
-            // generate new access token from refresh token add it to response
             ResponseCookie jwtCookie = jwtUtils.generateJwtCookie(jwtUtils.getRefreshTokenValidateAndGenerateAccessToken(request));
             response.addHeader("Set-Cookie", jwtCookie.toString());
-
-            // new content might be buggy
         }
+
         if (token != null && jwtUtils.validateJwtToken(token)) {
             String username = jwtUtils.getUserNameFromJwtToken(token);
             UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
