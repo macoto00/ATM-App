a310c7cc5924f55a24868633e8849865396d2a7e
TomasMacoun <macount@seznam.cz>
2023-09-21 15:15:46 +0200

    Create UserUpdateDTO.java

---
+++ BE/atmDemo/src/main/java/com/example/atmdemo/DTOs/UserUpdateDTO.java
@@ -0,0 +1,14 @@
+package com.example.atmdemo.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserUpdateDTO {
+    private String firstName;
+    private String lastName;
+    private String email;
+}
