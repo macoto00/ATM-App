dfc9974a381813366122890fff8614e99f8dff13
TomasMacoun <macount@seznam.cz>
2023-09-21 15:15:35 +0200

    Update UserAccountRepository.java

--- BE/atmDemo/src/main/java/com/example/atmdemo/repositories/UserAccountRepository.java
+++ BE/atmDemo/src/main/java/com/example/atmdemo/repositories/UserAccountRepository.java
@@ -1,8 +1,9 @@
 package com.example.atmdemo.repositories;
 
+import com.example.atmdemo.models.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserAccountRepository extends JpaRepository {
+public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
 }
