30059c508507af518bf35c5d67379c7ba8c02d7c
TomasMacoun <macount@seznam.cz>
2023-09-21 15:15:41 +0200

    Update User.java

--- BE/atmDemo/src/main/java/com/example/atmdemo/models/User.java
+++ BE/atmDemo/src/main/java/com/example/atmdemo/models/User.java
@@ -13,22 +13,12 @@ import java.util.List;
 @Data
 public class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "first_name")
     private String firstName;
-    @Column(name = "last_name")
     private String lastName;
-    @Column(name = "email")
     private String email;
-    @OneToMany(mappedBy = "user")
-    private List<UserAccount> accounts;
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
+    private String password;
+//    @OneToMany(mappedBy = "user")
+//    private List<UserAccount> accounts;
 }
