87a575cc2e45a4c4a63f10fb96754967c7c4b270
TomasMacoun <macount@seznam.cz>
2023-09-21 15:15:25 +0200

    Update UserService.java

--- BE/atmDemo/src/main/java/com/example/atmdemo/services/UserService.java
+++ BE/atmDemo/src/main/java/com/example/atmdemo/services/UserService.java
@@ -1,4 +1,17 @@
 package com.example.atmdemo.services;
 
+import com.example.atmdemo.DTOs.UserCreationDTO;
+import com.example.atmdemo.DTOs.UserUpdateDTO;
+import com.example.atmdemo.models.User;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface UserService {
+    void createUser(UserCreationDTO userCreationDTO);
+    Optional<User> getUserById(Long id);
+    List<User> getAllUsers();
+    void updateUser(UserUpdateDTO userUpdateDTO, Long id);
+    void deleteUser(Long id);
+    boolean userExistsByEmail(String email);
 }
