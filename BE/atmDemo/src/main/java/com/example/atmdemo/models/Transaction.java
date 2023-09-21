e33d41dd1fd3ec30c2bdc92fc86414bef3d2fecf
TomasMacoun <macount@seznam.cz>
2023-09-21 15:15:43 +0200

    Update Transaction.java

--- BE/atmDemo/src/main/java/com/example/atmdemo/models/Transaction.java
+++ BE/atmDemo/src/main/java/com/example/atmdemo/models/Transaction.java
@@ -14,23 +14,12 @@ import java.time.LocalDateTime;
 @Data
 public class Transaction {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "transaction_type")
     private String transactionType;
-    @Column(name = "amount")
     private BigDecimal amount;
-    @Column(name = "date")
     private LocalDateTime date;
-    @ManyToOne
-    @JoinColumn(name = "user_account_id")
-    private UserAccount userAccount;
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
+//    @ManyToOne
+//    @JoinColumn(name = "user_account_id")
+//    private UserAccount userAccount;
 }
