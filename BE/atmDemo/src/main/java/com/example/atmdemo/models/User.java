8b82ba986513ccb6b631d26320117a63da3a882a
TomasMacoun <macount@seznam.cz>
2023-09-21 19:08:57 +0200

    Update User.java

--- BE/atmDemo/src/main/java/com/example/atmdemo/models/User.java
+++ BE/atmDemo/src/main/java/com/example/atmdemo/models/User.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.UUID;
 
 @Entity
 @AllArgsConstructor
@@ -17,8 +18,19 @@ public class User {
     private Long id;
     private String firstName;
     private String lastName;
+    private String nickname;
     private String email;
     private String password;
+    private UUID uuid;
+    private String verificationToken;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = {@JoinColumn(name = "users_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "roles_id", referencedColumnName = "id")})
+    private List<Role> roles;
+
 //    @OneToMany(mappedBy = "user")
 //    private List<UserAccount> accounts;
 }
