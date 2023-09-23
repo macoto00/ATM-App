7bc68a4886369f837bb0fc8cd9b231d8e6e2d11f
TomasMacoun <macount@seznam.cz>
2023-09-23 18:27:25 +0200

    Update app-routing.module.ts

--- FE/atmAppFrontend/src/app/app-routing.module.ts
+++ FE/atmAppFrontend/src/app/app-routing.module.ts
@@ -4,12 +4,18 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { FaqComponent } from './components/faq/faq.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'faq', component: FaqComponent },
 ];
 
 @NgModule({
